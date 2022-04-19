0fa659ea9f08534d1f10f16c009d7deb01eca691
Yusuf Avcilar <yusufavcilar53@gmail.com>
2022-04-19 20:55:55 +0300

    javascript grade calculation project completed

---
+++ app.js
@@ -0,0 +1,37 @@
+const input1 = document.querySelector(".input1")
+const input2 = document.querySelector(".input2")
+const input3 = document.querySelector(".input3")
+const btnHesapla = document.querySelector(".btn")
+const result = document.querySelector(".result")
+
+let lesson1 = null
+let lesson2 = null
+let lesson3 = null
+
+input1.addEventListener("input", (event) => {
+   lesson1 = event.target.value
+})
+
+input2.addEventListener("input", (event) => {
+    lesson2 = event.target.value
+})
+
+input3.addEventListener("input", (event) =>{
+    lesson3 = event.target.value
+})
+
+btnHesapla.addEventListener ("click" ,() => {
+ const result1 = (Number(lesson1) + Number(lesson2) + Number(lesson3)) / 3;
+
+  result.innerHTML = result1;
+
+  input1.value = ""
+  input2.value = ""
+  input3.value = ""
+
+})
+
+
+
+
+
